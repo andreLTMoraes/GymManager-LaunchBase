d391e739071fb52fc410dba549d473cca4c2c331
ANdré Moraes <andre-altm@hotmail.com>
2020-04-28 19:12:58 -0300

    Função para atualizar instrutores

--- instructors.js
+++ instructors.js
@@ -71,3 +71,31 @@ exports.edit = function(req, res) {
 
     return res.render("instructors/edit", { instructor })
 }
+
+exports.update = function (req, res) {
+    const { id } = req.body
+    let index = 0
+
+    const foundInstructor = data.instructors.find(function(item, foundIndex){
+        if(id == item.id){
+            index = foundIndex
+            return true
+        }
+    })
+
+    if(!foundInstructor) return res.send("não encontrado")
+    
+    const instructor = {
+        ...foundInstructor,
+        ...req.body,
+        birth: Date.parse(req.body.birth)
+    }
+
+    data.instructors[index] = instructor
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
+        if(err) return res.send("Write error.")
+
+        return res.redirect(`/instructors/${id}`)
+    })
+}
--- routes.js
+++ routes.js
@@ -10,6 +10,7 @@ routes.get("/instructors/create", function(req, res) {
     return res.render("instructors/create")
 })
 
+routes.put("/instructors", instructors.update)
 routes.get("/instructors/:id", instructors.index)
 routes.get("/instructors/:id/edit", instructors.edit)
 routes.post("/instructors", instructors.create)
